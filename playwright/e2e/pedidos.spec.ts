3ef0962ea6509425374a3d52ed6035985352401a
Daniel Galvão <danielnicks@gmail.com>
2026-02-09 19:37:50 -0300

    iniciando automação de testes para consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  // Checkpoints
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible()
+
+  // Consultar pedido
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  // Checkpoints
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
+
+  // Pesquisar pedido
+  await page.getByTestId('search-order-id').click();
+  await page.getByTestId('search-order-id').fill('VLO-22GZHG');
+  await page.getByTestId('search-order-button').click();
+
+  // Resultado esperado
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-22GZHG');
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.getByTestId('order-result-status')).toBeVisible(); 
+
+  
+});
