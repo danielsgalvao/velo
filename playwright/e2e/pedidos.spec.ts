7ae56c15a5065d563ea9df1389ecb1d718ff3747
Daniel Galvão <danielnicks@gmail.com>
2026-02-10 12:44:49 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,29 +1,31 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('http://localhost:5173/')
 
-  // Checkpoints
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+/// AAA - Arrange, Act, Assert
+
+  // Checkpoints Arrange
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
   await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible()
 
   // Consultar pedido
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
 
-  // Checkpoints
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-  await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
+  // Checkpoints Act
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible()
 
   // Pesquisar pedido
-  await page.getByTestId('search-order-id').click();
-  await page.getByTestId('search-order-id').fill('VLO-22GZHG');
-  await page.getByTestId('search-order-button').click();
+  await page.getByTestId('search-order-id').click()
+  await page.getByTestId('search-order-id').fill('VLO-22GZHG')
+  await page.getByTestId('search-order-button').click()
 
-  // Resultado esperado
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-22GZHG');
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-  await expect(page.getByTestId('order-result-status')).toBeVisible(); 
+  // Resultado esperado Assert
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-22GZHG')
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByTestId('order-result-status')).toBeVisible() 
 
-  
-});
+
+})
